bebec1d4622c6749b550a82b1ff974f719378a7d
Jake Godsall <jake.edward.godsall@gmail.com>
2024-09-30 16:10:09 +0100

    added first System.Net.Http HTTP request/response example

--- TogglTrackAnalyser/Program.cs
+++ TogglTrackAnalyser/Program.cs
@@ -1,3 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
 
-Console.WriteLine("Hello, World!");
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        using var client = new HttpClient();
+        // Get the username and password from environment variables
+        var username = Environment.GetEnvironmentVariable("TOGGL_USERNAME");
+        var password = Environment.GetEnvironmentVariable("TOGGL_PASSWORD");
+        // Encode the username and password to base64
+        var byteArray = Encoding.ASCII.GetBytes($"{username}:{password}");
+        var base64String = Convert.ToBase64String(byteArray);
+        // add request headers
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", base64String);
+        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("accept/json"));
+        // make the GET request
+        var response = await client.GetAsync("https://api.track.toggl.com/api/v9/me/time_entries/current");
+        if (response.IsSuccessStatusCode)
+        {
+            var responseData = await response.Content.ReadAsStringAsync();
+            var responseStatus = response.StatusCode;
+            Console.WriteLine(responseStatus);
+            Console.WriteLine(responseData);
+        }
+    }
+}
